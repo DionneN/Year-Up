7920f66c2c41f69418116849f08a20568157de17
DionneN <Dnavarrete@atl.yearup.org>
2015-09-22 12:01:40 -0400

    you

---
+++ Project1/src/Intro.java
@@ -0,0 +1,13 @@
+
+public class Intro {
+
+	public static void main(String[] args) 
+	{
+		
+			System.out.println("My name is Dionne");
+			System.out.println("My favorite book is Jane Eyre");
+			System.out.println("I like to draw my rabbit");
+			
+			}
+
+}
