5b9f5ce8d18d18ed998cb8a6d8d5eb987ed7d6d4
dnavarrete <Dnavarrete@atl.yearup.org>
2015-10-09 08:25:42 -0400

    Tip Calculator

---
+++ EncapsulationLab/src/BillTipCalculator.java
@@ -0,0 +1,16 @@
+
+public class BillTipCalculator 
+{
+
+	public static void main(String[] args) {
+		System.out.println("$150.00");
+		Scanner sc = new Scanner(System.in;
+		double checkAmount = sc.nextDouble();
+		
+		double tip15 = 6.5980;
+		System.out.format("The 15 percent tip for this check is %3.3f%n", tip15)
+		}
+
+	}
+
+}
