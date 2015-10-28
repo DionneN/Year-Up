01a0f3774c53e601930e3caa996e9bb9c55e8814
dnavarrete <Dnavarrete@atl.yearup.org>
2015-10-28 16:07:43 -0400

    submit

---
+++ CalculateArearLab/src/CalculateArea.java
@@ -0,0 +1,41 @@
+
+public class CalculateArea 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("I am going to calculate the area of a circle");
+		double circleAreaIntoMethod = CalculateCircleArea(10);
+		System.out.println("Area of the circle is " + circleAreaIntoMethod);
+		
+		System.out.println("I am going to calculate the area of a rectangle");
+		double rectangleAreaIntoMethod = AreaRect(int 7, int3);
+		System.out.println("Area of the rectangle is " + rectangleAreaIntoMethod);
+		
+	}
+
+    public static double CalculateCircleArea(int radius)
+    {
+    	//This method calculates the area of a circle
+        //Input:  Integer radius value
+    	//Output: double area
+    	
+    	double area = 3.14 * radius * radius;
+    	return area;
+    }
+    
+    public static double AreaCircle(int radius);
+        //This method calculates the area of a circle
+        //Input: 5;
+        //Input:3;
+        //Output: double area
+    
+        double area = 3.14 * 5 * 3;
+        return area;
+        
+        
+    public static double AreaRect(int width,int length)
+        double area= length
+        }
+}
+
